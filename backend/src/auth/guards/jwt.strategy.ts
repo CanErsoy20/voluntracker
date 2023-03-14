9a0be0b45cc2bb56379b6f2496d6cbabe835ac4d
Tolga Özgün <tolgaozgunn@gmail.com>
2023-03-14 20:44:38 +0300

    Add JWT Strategy Fix

--- backend/src/auth/guards/jwt.strategy.ts
+++ backend/src/auth/guards/jwt.strategy.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConstants } from 'src/config';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.accessSecret,
+      secretOrKey: configService.get<string>('JWT_ACCESS_SECRET'),
     });
   }
 
