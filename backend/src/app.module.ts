fa111927247a1101cb93e316aa80f4baf8a4fddd
Tolga Özgün <tolgaozgunn@gmail.com>
2023-03-22 19:41:20 +0300

    Add Notifications

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { HelpZonesModule } from './help-zones/help-zones.module';
+import { NotificationsModule } from './notifications/notifications.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, HelpZonesModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, HelpZonesModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
