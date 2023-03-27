44043c8b6a80132a1da4fccbf256c238934ab8f5
CanErsoy20 <can.ersoy@ug.bilkent.edu.tr>
2023-03-27 18:02:49 +0300

    login screen design, api and bloc connections are done. UserModel should be implemented

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -4,8 +4,10 @@ import 'package:afet_takip/constants/api_constants.dart';
 import 'package:http/http.dart';
 
 import '../api.dart';
+import '../models/auth/login_model.dart';
 import '../models/auth/sign_up_model.dart';
 import '../models/auth/tokens_model.dart';
+import '../models/response_model.dart';
 
 class AuthService {
   AuthService();
@@ -19,6 +21,21 @@ class AuthService {
         return Tokens.fromJson(body);
       } else {
         return null;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<void> login(LoginModel bodyModel) async {
+    try {
+      Response? response;
+      response = await Api.instance.postRequest(ApiConstant.baseUrl,
+          ApiConstant.login, jsonEncode(bodyModel.toJson()));
+      if (response.statusCode == 200) {
+        dynamic body = jsonDecode(response.body);
+        ResponseModel responseModel = ResponseModel.fromJson(body);
+        //return UserModel.fromJson(responseModel.data);
       }
     } catch (e) {
       return null;
