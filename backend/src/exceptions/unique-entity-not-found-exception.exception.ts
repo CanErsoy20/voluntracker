2e261df252cfd5e221c5a8de6c692221ba7a9f3d
Selim Can Guler <selimcangler@gmail.com>
2023-03-28 20:27:39 +0300

    fixed a bug where the response structure of custom exceptions failed to pass the object correctly

--- backend/src/exceptions/unique-entity-not-found-exception.exception.ts
+++ backend/src/exceptions/unique-entity-not-found-exception.exception.ts
@@ -2,6 +2,6 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class UniqueEntityNotFoundException extends HttpException {
   constructor(message: string) {
-    super(message, HttpStatus.NOT_FOUND);
+    super({ message }, HttpStatus.NOT_FOUND);
   }
 }
