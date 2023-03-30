b5b9a2e9daab86c941c625fb7c8620f43845cb8c
Selim Can Guler <selimcangler@gmail.com>
2023-03-30 04:52:26 +0300

    multiple volunteer assign

--- backend/src/app/help-zones/help-centers/help-centers.module.ts
+++ backend/src/app/help-zones/help-centers/help-centers.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from 'src/authentication/auth.module';
-import { AuthService } from 'src/authentication/auth.service';
+import { UsersModule } from 'src/app/users/users.module';
 import { AuthorizationModule } from 'src/authorization/authorization.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { NeededSupplyService } from '../needed-supply/needed-supply.service';
@@ -20,6 +19,6 @@ import { HelpCentersService } from './help-centers.service';
     VolunteerService,
     VolunteerTeamService,
   ],
-  imports: [PrismaModule, VolunteerModule, AuthorizationModule],
+  imports: [PrismaModule, VolunteerModule, AuthorizationModule, UsersModule],
 })
 export class HelpCentersModule {}
--- backend/src/app/qr-generator/qr-generator.module.ts
+++ backend/src/app/qr-generator/qr-generator.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { QrGeneratorService } from './qr-generator.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { QrGeneratorController } from './qr-generator.controller';
+import { QrGeneratorService } from './qr-generator.service';
 
 @Module({
   providers: [QrGeneratorService],
-  controllers: [QrGeneratorController]
+  controllers: [QrGeneratorController],
+  imports: [PrismaModule],
+  exports: [QrGeneratorService],
 })
 export class QrGeneratorModule {}
