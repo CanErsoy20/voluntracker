5383373d01a5a68a31fbfc8982890ba5fa3f862e
Tolga Özgün <tolgaozgunn@gmail.com>
2023-03-31 16:11:50 +0300

    Make User Bar clickable

--- frontend/lib/view/widgets/user_bar.dart
+++ frontend/lib/view/widgets/user_bar.dart
@@ -1,4 +1,5 @@
 import 'package:afet_takip/models/user/user_model.dart';
+import 'package:afet_takip/router.dart';
 import 'package:flutter/material.dart';
 
 class UserBar extends StatelessWidget {
@@ -11,65 +12,70 @@ class UserBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 120,
-      // color: Colors.white,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(8, 8, 15, 8),
-            child: CircleAvatar(
-              radius: 30,
-              // backgroundImage: NetworkImage(user.ava),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, Routes.profile);
+      },
+      child: Container(
+        height: 120,
+        // color: Colors.white,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(8, 8, 15, 8),
+              child: CircleAvatar(
+                radius: 30,
+                // backgroundImage: NetworkImage(user.ava),
+              ),
             ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "${user.firstname} ${user.surname}",
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${user.firstname} ${user.surname}",
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  user.getHighestRole(),
-                  style: const TextStyle(
-                    fontSize: 14,
+                  const SizedBox(height: 4),
+                  Text(
+                    user.getHighestRole(),
+                    style: const TextStyle(
+                      fontSize: 14,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  "Team 1",
-                  style: const TextStyle(
-                    fontSize: 14,
+                  const SizedBox(height: 4),
+                  Text(
+                    "Team 1",
+                    style: const TextStyle(
+                      fontSize: 14,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.person,
-              color: Colors.white,
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
             ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.logout,
-              color: Colors.white,
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.logout,
+                color: Colors.white,
+              ),
             ),
-          ),
-          const SizedBox(width: 8),
-        ],
-      ),
+            const SizedBox(width: 8),
+          ],
+        ),
+      )
     );
   }
 }
