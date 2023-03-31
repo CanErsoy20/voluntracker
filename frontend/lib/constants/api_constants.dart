9871ec47070434ea87cedf9d6af7745cb2437143
Tolga Özgün <tolgaozgunn@gmail.com>
2023-03-31 08:22:32 +0300

    Change base URL

--- frontend/lib/constants/api_constants.dart
+++ frontend/lib/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstant {
-  static String baseUrl = "http://92.205.189.130/api/v1/"; //main
+  static String baseUrl = "https://api.voluntracker.app/v1/"; //main
   //static String baseUrl = "http://92.205.26.185/api/v1";
   static String helpCenters = "helpCenters/";
   static String neededVolunteers = "neededVolunteers/";
