e61309bb75a4ef1913d8f2734083ccdc5de58b61
Selim Can Guler <selimcangler@gmail.com>
2023-04-01 12:16:08 +0300

    updated stuff

--- backend/src/app/users/users.controller.ts
+++ backend/src/app/users/users.controller.ts
@@ -1,7 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { HttpResponse } from 'src/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
