032bbf7e8fe2cf8b7b114aaac025eb89a6969821
Selim Can Guler <selimcangler@gmail.com>
2023-04-04 16:10:41 +0300

    volunteer team leader create, remove

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AssetsModule } from './app/assets/assets.module';
 import { HelpZonesModule } from './app/help-zones/help-zones.module';
 import { NotificationsModule } from './app/notifications/notifications.module';
 import { QrGeneratorModule } from './app/qr-generator/qr-generator.module';
@@ -14,6 +15,7 @@ import { PrismaModule } from './prisma/prisma.module';
     HelpZonesModule,
     NotificationsModule,
     QrGeneratorModule,
+    AssetsModule,
   ],
   controllers: [],
 })
